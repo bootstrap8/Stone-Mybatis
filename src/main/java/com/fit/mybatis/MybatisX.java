ec514f37aca28a4f8eac35f40c9e9d89a94b799f
zhaoyy8 <stone@malou123>
2021-09-12 11:29:02 +0800

    MybatisX

---
+++ src/main/java/com/fit/mybatis/MybatisX.java
@@ -0,0 +1,14 @@
+package com.fit.mybatis;
+
+/**
+ * @author Stone
+ * @version 1.0
+ * @since 2021-09-12
+ */
+public class MybatisX {
+
+  public static void main(String[] args) {
+    System.out.println("Action Now!");
+  }
+
+}
